9c98262ff03f2688e41d938f2c8d7a11275bc02b
gsandhu916 <gsandhu916@gmail.com>
2019-06-05 19:43:10 -0700

    took out proxy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
