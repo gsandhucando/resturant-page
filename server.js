5e2f7f91318a9ccda8c68cf8becbb189bf2e0f06
gsandhu916 <gsandhu916@gmail.com>
2019-06-05 19:52:38 -0700

    build setup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 
 dotenv.config({
   path: '.env'
@@ -12,6 +13,12 @@ dotenv.config({
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 
 //Import Routes
 const postsRoutes = require('./routes/reservations');
